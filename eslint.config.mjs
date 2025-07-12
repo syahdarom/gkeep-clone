3e65642ca1f43a9a97a315a9066f083b95a55aa4
Syahda Romansyah <syahdarom@outlook.com>
2025-07-12 14:39:46 +0700

    chore: set up an ESLint config for root repo

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import json from '@eslint/json';
+import eslintCfgPrettier from 'eslint-config-prettier/flat';
+import eslintPlgUnusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['.vercel/', '.turbo/', 'apps/', 'packages/']),
+  {
+    files: ['**/*.{js,mjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.{js,mjs,ts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    plugins: {
+      'unused-imports': eslintPlgUnusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  eslintCfgPrettier,
+]);
