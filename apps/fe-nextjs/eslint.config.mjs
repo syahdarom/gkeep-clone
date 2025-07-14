b6d2a25e03cce0510eb930ba4f175d15822dd495
Syahda Romansyah <syahdarom@outlook.com>
2025-07-14 13:38:21 +0700

    chore: update the ESLint config
    Update the ESLint configuration for the apps/fe-nextjs package.

--- apps/fe-nextjs/eslint.config.mjs
+++ apps/fe-nextjs/eslint.config.mjs
@@ -1,4 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import eslintCfgPrettier from 'eslint-config-prettier';
+import eslintPlgTurbo from 'eslint-plugin-turbo';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -10,7 +12,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  eslintPlgTurbo.configs['flat/recommended'],
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintCfgPrettier,
 ];
 
 export default eslintConfig;
